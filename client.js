ce2842fc7c0a03f3abfc3970f0adf71e3ae7fd5c
mehtaReena <j.reena07@gmail.com>
2021-01-07 19:21:56 +0530

    Commit

--- client.js
+++ client.js
@@ -1,6 +1,47 @@
-const io = require("socket.io-client");
+const io = require("socket.io-client")('http://localhost:3000'); ;
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin,  output: process.stdout });
 
-// write your code here
 
+var name;
+//
+// write your code here
+var socket = io.connect();
+
+
+socket.on('connect', () => {
+    if(name==undefined){
+        rl.question("Please enter a your name: ", function(user) {
+            name = user;
+            var msg = name + " has joined the chat"; 
+            io.emit('simple chat message',msg);
+            console.log(msg);   
+            rl.prompt(true);
+        });
+
+    }    
+   
+  });
+
+socket.on('disconnect', () => {
+    console.log('Connection lost...!');
+});
+
+
+// Set the username
+
+
+
+socket.on('simple chat message', function (data) {
+    console.log(data);  
+    rl.prompt(true);  
+  });
+
+
+
+rl.on('line', function (line) {       
+         // send chat message
+        io.emit('simple chat message',name+": "+line);   
+        rl.prompt(true);
+             
+    });
--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 io.on('connection', (socket) => {
+
     console.log('a user connected');
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
     let eventName = 'simple chat message';
     let broadcast = (msg) => socket.broadcast.emit(eventName, msg);
-    socket.on(eventName, (msg, ackFn) => {
+    socket.on(eventName, (msg) => {
         console.log('message: ' + msg);
         // broadcast to other clients after 1.5 seconds
         setTimeout(broadcast, 1500, msg);
