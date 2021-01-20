34247e36fca03343868e537880e43869797bc321
mehtaReena <j.reena07@gmail.com>
2021-01-20 21:29:47 +0530

    commit

--- client.js
+++ client.js
@@ -4,6 +4,7 @@ const rl = readline.createInterface({ input: process.stdin,  output: process.std
 
 
 var name;
+var id;
 //
 // write your code here
 var socket = io.connect();
@@ -11,35 +12,81 @@ var socket = io.connect();
 
 socket.on('connect', () => {
     if(name==undefined){
-        rl.question("Please enter a your name: ", function(user) {
+        rl.question("What is your name? ", function(user) {
+            console.log("Successfully connected to server");  
             name = user;
-            var msg = name + " has joined the chat"; 
-            io.emit('simple chat message',msg);
-            console.log(msg);   
+            id=socket.id;
+            var msg = name + " has joined the game"; 
+           // io.emit('simple chat message',{msg,id});             
             rl.prompt(true);
+            askChoice();
+            
         });
 
+       
     }    
    
   });
 
   
-rl.on('line', function (line) {       
+/* rl.on('line', function (line) {       
     // send chat message
    io.emit('simple chat message',name+": "+line);   
    rl.prompt(true);
         
-});
+}); */
 
 
 socket.on('disconnect', () => {
     console.log('Connection lost...!');
 });
 
+function askChoice(){
+rl.question("R)ock, (P)aper or (S)cissors? ", function(option){
+    if(option.toLowerCase()==='r'){
+        option="Rock";
+    }
+    if(option.toLowerCase()==='p'){
+        option="Paper";
+    }
+    if(option.toLowerCase()==='s'){
+        option="Scissors";
+    }
+    console.log("You chose "+ option );
+    console.log("Waiting for response");
+     io.emit('player choice',id,name,option);
+
+    });
+
+}
+
 
 socket.on('simple chat message', function (data) {
-    console.log(data);  
-    rl.prompt(true);  
+    
+    if(data) {
+        console.log(data); 
+       
+    }
+       
+    
+     
   });
+
+  socket.on('player2', function (data) {
+    if(data) {
+        console.log(data); 
+        askChoice();
+    }
+    
+  });
+
+  socket.on('player1', function (data) {
+    if(data) {
+        console.log(data); 
+        askChoice();
+    } 
+  });
+
+  
 
 
--- index.js
+++ index.js
@@ -2,20 +2,118 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-io.on('connection', (socket) => {
 
+users = [];
+connections = [];
+choices = [];
+
+io.on('connection', (socket) => {   
     console.log('a user connected');
-
+    
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
     let eventName = 'simple chat message';
     let broadcast = (msg) => socket.broadcast.emit(eventName, msg);
-    socket.on(eventName, (msg) => {
-        console.log('message: ' + msg);
+
+    socket.on(eventName, (msg) => {        
+        console.log('message: ' + msg.msg );
         // broadcast to other clients after 1.5 seconds
-        setTimeout(broadcast, 1500, msg);
+        setTimeout(broadcast, 1500, msg.msg);
     });
+
+    socket.on('player choice', function (userid,username, choice) {
+         choices.push({'userid': userid,'user': username, 'choice': choice.toLowerCase()});
+         console.log( username+" says "+ choice);
+         if(choices.length == 2) {            
+           
+             let player1ID=socketId=choices[0]['userid']; 
+             let player2ID=socketId=choices[1]['userid']; 
+
+             switch (choices[0]['choice'])
+             {
+                 case 'rock':
+                     switch (choices[1]['choice'])
+                     {
+                         case 'rock':                             
+                             io.to(player1ID).emit('player1', choices[1]['user'] + " chose " +choices[1]['choice']+' - Draw'); 
+                             io.to(player2ID).emit('player2', choices[0]['user'] + " chose " +choices[0]['choice']+' - Draw '); 
+                             break;
+ 
+                         case 'paper':                          
+                            io.to(player2ID).emit('player2', choices[0]['user'] + " chose " +choices[0]['choice']+' - You win :)');   
+                            io.to(player1ID).emit('player1', choices[1]['user'] + " chose " +choices[1]['choice']+' - You loss :('); 
+                                        
+                             break;
+         
+                         case 'scissors':                            
+                             io.to(player1ID).emit('player1', choices[1]['user'] + " chose " +choices[1]['choice']+' -You win :)');                           
+                             io.to(player2ID).emit('player2', choices[0]['user'] + " chose " +choices[0]['choice']+' - You loss :('); 
+                              
+                             break;
+ 
+                         default:
+                             break;
+                     }
+                     break;
+ 
+                 case 'paper':
+                     switch (choices[1]['choice'])
+                     {
+                         case 'rock':                           
+                            io.to(player1ID).emit('player1', choices[1]['user'] + " chose " +choices[1]['choice']+' -You win :)');                           
+                            io.to(player2ID).emit('player2', choices[0]['user'] + " chose " +choices[0]['choice']+' -You loss :(');                            
+                             break;
+ 
+                         case 'paper':
+                             io.to(player1ID).emit('player1', choices[1]['user'] + " chose " +choices[1]['choice']+' - Draw'); 
+                             io.to(player2ID).emit('player2', choices[0]['user'] + " chose " +choices[0]['choice']+' - Draw '); 
+                             break;
+         
+                         case 'scissors':                          
+                            io.to(player2ID).emit('player2', choices[0]['user'] + " chose " +choices[0]['choice']+' -You win :)');                            
+                            io.to(player1ID).emit('player1', choices[1]['user'] + " chose " +choices[1]['choice']+' -You loss :(');                            
+                             break;
+ 
+                         default:
+                             break;
+                     }
+                 break;
+ 
+                 case 'scissors':
+                     switch (choices[1]['choice'])
+                     {
+                         case 'rock':                          
+                           io.to(player2ID).emit('player2', choices[0]['user'] + " chose " +choices[0]['choice']+' -You win :)');                          
+                           io.to(player1ID).emit('player1', choices[1]['user'] + " chose " +choices[1]['choice']+' -You loss :('); 
+                            
+                             break;
+ 
+                         case 'paper':                          
+                            io.to(player1ID).emit('player1', choices[1]['user'] + " chose " +choices[1]['choice']+' -You win :)');                           
+                            io.to(player2ID).emit('player2', choices[0]['user'] + " chose " +choices[0]['choice']+' -You loss :('); 
+                             
+                             break;
+         
+                         case 'scissors':
+                            io.to(player1ID).emit('player1', choices[1]['user'] + " chose " +choices[1]['choice']+' - Draw'); 
+                            io.to(player2ID).emit('player2', choices[0]['user'] + " chose " +choices[0]['choice']+' - Draw '); 
+                             break;
+ 
+                         default:
+                             break;
+                     }
+                     break;
+ 
+                 default:
+                     break;
+             }
+ 
+            choices=[];
+         }
+
+ });
+ 
 });
 
 http.listen(3000, () => {
